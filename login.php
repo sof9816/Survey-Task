9438b04d632b986ef465f3f8f8166a1f845e48e7
sof9816 <sof9816@users.noreply.github.com>
2018-09-07 21:26:18 +0300

    Update login.php

--- login.php
+++ login.php
@@ -3,8 +3,16 @@ include('file.php');
 session_start();
 include("config/connection.php");
 
+function encryptIt($q)
+{
+    $cryptKey = 'qJB0rGtIn5UB1xG03efyCp';
+    $qEncoded = @base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, md5($cryptKey), $q, MCRYPT_MODE_CBC, md5(md5($cryptKey))));
+    return ($qEncoded);
+}
+
 function chk($dbc, $user, $pass)
 {
+    $pass = encryptIt($pass);
     $q = 'select user_name,password from users where ( user_name="'
         . $user . '" or email ="'
         . $user . '" ) AND password="' . $pass . '" ;';
